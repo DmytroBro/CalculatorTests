c6cd84a1abd27769101198d1a0af801cfe4bc91c
Dmytro Kovtun <dmytrobrovar@gmail.com>
2021-06-15 21:38:58 +0300

    Testst for calculator to new repository

---
+++ src/test/java/tests/DivCalcTest.java
@@ -0,0 +1,11 @@
+package tests;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class DivCalcTest extends BaseTest{
+    @Test
+    public void tenDivideFiveTest() {
+        long result = calculator.div(10,5);
+        Assert.assertEquals(result, 2, "invalid divide operation result");
+    }
+}
